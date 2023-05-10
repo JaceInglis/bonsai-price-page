6f1b15d84bde6f887925fd5efb54a04eb2ff7b48
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-10 16:11:31 -0700

    Show dropdown menu based off screen size

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { AppBar, Toolbar, Box, Typography, Button, IconButton } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
+import { useTheme } from "@mui/material/styles";
 
 const Navbar = () => {
 
-  const dropDown = () => {
-
-  }
+  const theme = useTheme();
 
   return (
     <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'center' }}>
@@ -15,14 +14,19 @@ const Navbar = () => {
             <Typography variant='h6' sx={{ flexGrow: 1 }}>
                 bonsai
             </Typography>
-            <Box mr={10}>
-                <Button color='inherit'>Product</Button>
-                <Button color='inherit'>Templates</Button>
-                <Button color='inherit'>Pricing</Button>
-                <Button color='inherit'>Reviews</Button>
+            <Box sx={{ display: 'flex', [theme.breakpoints.down('md')]: {display: 'none'} }}>
+                <Box mr={10}>
+                    <Button color='inherit'>Product</Button>
+                    <Button color='inherit'>Templates</Button>
+                    <Button color='inherit'>Pricing</Button>
+                    <Button color='inherit'>Reviews</Button>
+                </Box>
+                <Button sx={{ mr: 2, borderRadius: '20px' }} color='inherit'>Log in</Button>
+                <Button sx={{ borderRadius: '20px' }} variant='contained' color='primary'>Start free</Button>
             </Box>
-            <Button sx={{ mr: 2, borderRadius: '20px' }} color='inherit'>Log in</Button>
-            <Button sx={{ borderRadius: '20px' }} variant='contained' color='primary'>Start free</Button>
+            <IconButton sx={{[theme.breakpoints.up('md')]: {display: 'none'}, color: 'white', backgroundColor: 'black', ':hover': {backgroundColor: 'darkgrey'}}}>
+                <MenuIcon />
+            </IconButton>
         </Toolbar>
       </AppBar>
     </Box>
