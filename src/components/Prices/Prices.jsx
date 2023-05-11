de8071d261364948cf7adf171a15da7295f7f838
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-11 15:01:24 -0700

    Build prices object

--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const prices = [
+    {name: 'Standard', priceMonth: 24, priceYear: 17, description: 'Ideal for freelancers and contractors just starting out. Billed annually.', features: ['Standard workflow']},
+    {name: 'Professional', priceMonth: 39, priceYear: 32, description: 'Everything a growing independent business needs to thrive.', features: ['Professional workflow']},
+    {name: 'Business', priceMonth: 79, priceYear: 52, description: 'The perfect package for small businesses and agencies.', features: ['Business workflow']}
+]
 
 const Prices = () => {
   return (
