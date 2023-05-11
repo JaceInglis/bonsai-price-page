e79c55e7dc8a2b500a9a238d9679dba0a67f5ed2
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-11 15:48:04 -0700

    Get prices and content on page with basic styles

--- src/components/Prices/Price/Price.jsx
+++ src/components/Prices/Price/Price.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Typography, Paper, Button, Divider, Box, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
 
-const Price = ({ name, price, description, features }) => {
+const Price = ({ name, price, description, features, slider }) => {
   return (
-    <div>Price</div>
+    <Paper sx={{ display: 'flex', flexDirection: 'column'}}>
+        <Typography variant='h6'>{name}</Typography>
+        <Typography variant='h6'>$ {price} {slider? '/year' : '/month'}</Typography>
+        <Button sx={{ textTransform: 'unset', width: '20%', borderRadius: '20px', backgroundColor: '#22ad01', ':hover': {backgroundColor: '#22af01'} }} size='large' variant='contained'>Start for free</Button>
+        <Typography variant='body2'>{description}<br />{slider && 'Billed annually.'}</Typography>
+        <Divider />
+        <List>
+            {features.map(item => (
+                <ListItem key={item}>
+                    <ListItemIcon>
+                        <CheckIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={item}/>
+                </ListItem>
+            ))}
+        </List>
+    </Paper>
   )
 }
 
--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
-import Price from './Price/Price'
+import { Box } from '@mui/material';
+import Price from './Price/Price';
 
 const prices = [
-    {name: 'Standard', priceMonth: 24, priceYear: 17, description: 'Ideal for freelancers and contractors just starting out. Billed annually.', features: ['Standard workflow']},
+    {name: 'Standard', priceMonth: 24, priceYear: 17, description: 'Ideal for freelancers and contractors just starting out.', features: ['Standard workflow']},
     {name: 'Professional', priceMonth: 39, priceYear: 32, description: 'Everything a growing independent business needs to thrive.', features: ['Professional workflow']},
     {name: 'Business', priceMonth: 79, priceYear: 52, description: 'The perfect package for small businesses and agencies.', features: ['Business workflow']}
 ]
@@ -12,9 +13,11 @@ const Prices = () => {
   const [slider, setSlider] = useState(false);
 
   return (
-    prices.map(price => (
-        <Price name={price.name} price={slider? price.priceYear : price.priceMonth} description={price.description} features={price.features} />
-    ))
+    <Box>
+        {prices.map(price => (
+            <Price key={price.name} name={price.name} price={slider? price.priceYear : price.priceMonth} description={price.description} features={price.features} slider={slider}/>
+        ))}
+    </Box>
   )
 }
 
