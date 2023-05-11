906e992598c8a098aefbc5a95e25fbf9e72ac593
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-11 14:48:03 -0700

    Create prices component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import Prices from './components/Prices/Prices';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Prices />
     </>
   );
 }
