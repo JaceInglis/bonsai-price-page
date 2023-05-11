cdf7e07d88d1997c26ef28af2ae89e8f31f7bf0a
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-11 16:11:13 -0700

    Put price obj in seprate file and add features

--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from 'react'
 import { Box, Typography } from '@mui/material';
 import Price from './Price/Price';
-
-const prices = [
-    {name: 'Standard', priceMonth: 24, priceYear: 17, description: 'Ideal for freelancers and contractors just starting out.', features: ['Standard workflow']},
-    {name: 'Professional', priceMonth: 39, priceYear: 32, description: 'Everything a growing independent business needs to thrive.', features: ['Professional workflow']},
-    {name: 'Business', priceMonth: 79, priceYear: 52, description: 'The perfect package for small businesses and agencies.', features: ['Business workflow']}
-]
+import { prices } from './priceObj';
 
 const Prices = () => {
 
---
+++ src/components/Prices/priceObj.js
@@ -0,0 +1,36 @@
+export const prices = [
+    {name: 'Standard', priceMonth: 24, priceYear: 17, description: 'Ideal for freelancers and contractors just starting out.', features: [
+        'Unlimited Clients & Projects',
+        'Invoicing & Payments',
+        'Proposal & Contracts',
+        'Scheduling',
+        'Tasks & Time Tracking',
+        'Client CRM',
+        'Forms & Questionnaires',
+        'Expense Tracking',
+        'Up to 5 Project Collaborators'
+      ]},
+    {name: 'Professional', priceMonth: 39, priceYear: 32, description: 'Everything a growing independent business needs to thrive.', features: [
+        'Everything in Starter, plus:',
+        'Custom Branding',
+        'Workflow Automations',
+        'Client Portal',
+        'Unlimited Scheduling events',
+        'QuickBooks Integration',
+        'Calendly Integration',
+        'Zapier Integration',
+        'Up to 15 Project Collaborators'
+      ]},
+    {name: 'Business', priceMonth: 79, priceYear: 52, description: 'The perfect package for small businesses and agencies.', features: [
+        'Everything in Professional, plus:',
+        'Subcontractor Management',
+        'Hiring Agreement Templates (1099 contractors)',
+        'Subcontractor Onboarding',
+        'Talent Pool',
+        '3 Team Seats, additional seats $9/month',
+        'Accountant Access',
+        'Connect Multiple Bank Accounts',
+        'Unlimited Subcontractors',
+        'Unlimited Project Collaborators'
+      ]}
+]
