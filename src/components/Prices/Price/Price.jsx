3b5158a119ebf1d7e8bbf5bc2edac6c15b3c1dce
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-18 20:48:45 -0700

    Add styles to price component

--- src/components/Prices/Price/Price.jsx
+++ src/components/Prices/Price/Price.jsx
@@ -5,21 +5,22 @@ import CheckIcon from '@mui/icons-material/Check';
 const Price = ({ name, price, description, features, slider }) => {
   return (
     <Paper sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center'}}>
-        <Typography variant='h4'>{name}</Typography>
-        <Typography variant='h6'>$ <strong>{price}</strong> /month</Typography>
-        <Button sx={{ textTransform: 'unset', borderRadius: '20px', backgroundColor: '#22ad01', ':hover': {backgroundColor: '#22af01'} }} size='large' variant='contained'>Start for free</Button>
-        <Typography variant='body2'>{description}<br />{slider && 'Billed annually.'}</Typography>
+        <Typography variant='h4' sx={{mt: '100px', mb: '15px', color: '#003c2f', fontWeight: '700'}}>{name}</Typography>
+        <Typography variant='h6' sx={{color: '#003c2f', fontWeight: '700'}}>$ <span style={{fontSize: '2.5rem'}}>{price}</span> <span style={{color: '#b2c0b6', fontSize: '1rem'}}>/month</span></Typography>
+        <Button sx={{ textTransform: 'unset', borderRadius: '50px', backgroundColor: '#22ad01', ':hover': {backgroundColor: '#22af01'}, m: '1.5rem 0rem', width: '12rem' }} size='large' variant='contained'>Start for free</Button>
+        <Typography sx={{width: '12rem', mb: '1rem'}} variant='body2'>{description}<br />{slider && 'Billed annually.'}</Typography>
         <List>
-        <Divider variant='middle'/>
+        <Divider sx={{mb: '1rem'}} variant='middle'/>
             {features.map(item => (
                 <ListItem key={item}>
                     <ListItemIcon>
-                        <CheckIcon />
+                        <CheckIcon sx={{color: '#22ad01'}}/>
                     </ListItemIcon>
                     <ListItemText primary={item}/>
                 </ListItem>
             ))}
         </List>
+        <Button sx={{ textTransform: 'unset', borderRadius: '50px', backgroundColor: '#22ad01', ':hover': {backgroundColor: '#22af01'}, width: '12rem', m: '2rem 0rem' }} size='large' variant='contained'>Start for free</Button>
     </Paper>
   )
 }
