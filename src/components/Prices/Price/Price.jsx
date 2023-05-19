4b574ac97f1e0e79ecc6601a50aaa3bddc613c03
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-18 21:13:29 -0700

    Add more styles to price

--- src/components/Prices/Price/Price.jsx
+++ src/components/Prices/Price/Price.jsx
@@ -8,7 +8,7 @@ const Price = ({ name, price, description, features, slider }) => {
         <Typography variant='h4' sx={{mt: '100px', mb: '15px', color: '#003c2f', fontWeight: '700'}}>{name}</Typography>
         <Typography variant='h6' sx={{color: '#003c2f', fontWeight: '700'}}>$ <span style={{fontSize: '2.5rem'}}>{price}</span> <span style={{color: '#b2c0b6', fontSize: '1rem'}}>/month</span></Typography>
         <Button sx={{ textTransform: 'unset', borderRadius: '50px', backgroundColor: '#22ad01', ':hover': {backgroundColor: '#22af01'}, m: '1.5rem 0rem', width: '12rem' }} size='large' variant='contained'>Start for free</Button>
-        <Typography sx={{width: '12rem', mb: '1rem'}} variant='body2'>{description}<br />{slider && 'Billed annually.'}</Typography>
+        <Typography sx={{width: '12rem', mb: '1rem', color: '#758e7c'}} variant='body2'>{description}<br />{slider && 'Billed annually.'}</Typography>
         <List>
         <Divider sx={{mb: '1rem'}} variant='middle'/>
             {features.map(item => (
@@ -16,7 +16,7 @@ const Price = ({ name, price, description, features, slider }) => {
                     <ListItemIcon>
                         <CheckIcon sx={{color: '#22ad01'}}/>
                     </ListItemIcon>
-                    <ListItemText primary={item}/>
+                    <ListItemText primaryTypographyProps={{fontSize: '1rem', color: '#213c36', fontWeight: 'bold'}} primary={item}/>
                 </ListItem>
             ))}
         </List>
