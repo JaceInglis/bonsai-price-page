10d4dacbe83ad4def54ca55a2859d38c81673993
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-29 11:48:13 -0700

    Add styles to prices and use grid

--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Grid } from '@mui/material';
 import Price from './Price/Price';
 import { prices } from './priceObj';
 import Slider from './Slider/Slider';
@@ -12,14 +12,16 @@ const Prices = () => {
   }
 
   return (
-    <Box sx={{ mt: '5%', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+    <Box sx={{ mt: '5%', display: 'flex', flexDirection: 'column', alignItems: 'center', justiftyContent: 'center'}}>
         <Typography variant='h2' sx={{ textAlign: 'center', width: '50%', mb: '5%', color: '#003c2f', fontWeight: '700'}}>Pick the best plan for your business</Typography>
         <Slider handleToggle={handleToggle} slider={slider}/>
-        <Box sx={{display: 'flex', mt: '5%'}}>
+        <Grid container spacing={2} sx={{ flexGrow: 1, mt: '5%', justifyContent: 'center' }}>
             {prices.map(price => (
-                <Price key={price.name} name={price.name} price={slider? price.priceYear : price.priceMonth} description={price.description} features={price.features} slider={slider}/>
-            ))}
-        </Box>
+                <Grid item xs={10} md={3.5} sm={10} lg={2.5}>
+                    <Price key={price.name} name={price.name} price={slider? price.priceYear : price.priceMonth} description={price.description} features={price.features} slider={slider}/>
+                </Grid>
+            ))}   
+        </Grid>
     </Box>
   )
 }
