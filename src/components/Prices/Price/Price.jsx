2e574df7e8e905dd9b63895835b4c247972a34ec
Jace Inglis <62110248+JaceInglis@users.noreply.github.com>
2023-05-30 23:26:25 -0700

    Add best value outline to prices

--- src/components/Prices/Price/Price.jsx
+++ src/components/Prices/Price/Price.jsx
@@ -4,7 +4,7 @@ import CheckIcon from '@mui/icons-material/Check';
 
 const Price = ({ name, price, description, features, slider }) => {
   return (
-    <Paper elevation={5} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', borderRadius: '20px'}}>
+    <Paper elevation={5} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center', borderRadius: '20px'}}>
         <Typography variant='h4' sx={{mt: '100px', mb: '15px', color: '#003c2f', fontWeight: '700'}}>{name}</Typography>
         <Typography variant='h6' sx={{color: '#003c2f', fontWeight: '700'}}>$ <span style={{fontSize: '2.5rem'}}>{price}</span> <span style={{color: '#b2c0b6', fontSize: '1rem'}}>/month</span></Typography>
         <Button sx={{ textTransform: 'unset', borderRadius: '50px', backgroundColor: '#22ad01', ':hover': {backgroundColor: '#22af01'}, m: '1.5rem 0rem', width: '12rem' }} size='large' variant='contained'>Start for free</Button>
--- src/components/Prices/Prices.jsx
+++ src/components/Prices/Prices.jsx
@@ -3,6 +3,7 @@ import { Box, Typography, Grid } from '@mui/material';
 import Price from './Price/Price';
 import { prices } from './priceObj';
 import Slider from './Slider/Slider';
+import { useTheme } from "@mui/material/styles";
 
 const Prices = () => {
   const [slider, setSlider] = useState(false);
@@ -11,14 +12,23 @@ const Prices = () => {
     setSlider(!slider)
   }
 
+  const theme = useTheme();
+
   return (
     <Box sx={{ mt: '5%', display: 'flex', flexDirection: 'column', alignItems: 'center', justiftyContent: 'center'}}>
         <Typography variant='h2' sx={{ textAlign: 'center', width: '50%', mb: '5%', color: '#003c2f', fontWeight: '700'}}>Pick the best plan for your business</Typography>
         <Slider handleToggle={handleToggle} slider={slider}/>
-        <Grid container spacing={2} sx={{ flexGrow: 1, mt: '5%', justifyContent: 'center' }}>
+        <Grid container spacing={3} sx={{ flexGrow: 1, mt: '5%', justifyContent: 'center' }}>
             {prices.map(price => (
                 <Grid item xs={10} md={3.5} sm={10} lg={2.5}>
+                    {price.name === 'Professional'?
+                    <Box sx={{background: '#ee8146', padding: '.5em', textAlign: 'center', borderRadius: '20px', [theme.breakpoints.up('md')]: {position: 'relative', bottom: '2.5em'}}}>
+                      <Typography variant='subtitle2' sx={{color: '#fff', mb: '10px'}}>Best value for money</Typography>
+                      <Price key={price.name} name={price.name} price={slider? price.priceYear : price.priceMonth} description={price.description} features={price.features} slider={slider}/>
+                    </Box>
+                    :
                     <Price key={price.name} name={price.name} price={slider? price.priceYear : price.priceMonth} description={price.description} features={price.features} slider={slider}/>
+                    }
                 </Grid>
             ))}   
         </Grid>
